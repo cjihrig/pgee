c3533e846630f946cfd4f46d7d2d6f4558e1cdd2
cjihrig <cjihrig@gmail.com>
2016-01-31 22:58:57 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,104 @@
 # pgee
-PostgreSQL Asynchronous Notification Event Emitter
+
+[![Current Version](https://img.shields.io/npm/v/pgee.svg)](https://www.npmjs.org/package/pgee)
+[![Build Status via Travis CI](https://travis-ci.org/continuationlabs/pgee.svg?branch=master)](https://travis-ci.org/continuationlabs/pgee)
+![Dependencies](http://img.shields.io/david/continuationlabs/pgee.svg)
+
+[![belly-button-style](https://cdn.rawgit.com/continuationlabs/belly-button/master/badge.svg)](https://github.com/continuationlabs/belly-button)
+
+PostgreSQL asynchronous notification event emitter. `pgee` allows PostgreSQL's `LISTEN` and `NOTIFY` features to be used as a Node.js `EventEmitter`.
+
+An example use of `pgee` is shown below. `pgee` exports a single constructor that extends `EventEmitter`. The constructor accepts anything that can be passed to [`pg.connect()`](https://www.npmjs.com/package/pg), or an already connected `pg.Client` instance. The `pgee.prototype.listen()` and `pgee.prototype.unlisten()` methods are used to `LISTEN` and `UNLISTEN` channels in the database. Notifications received from the database are emitted as events. Similarly, the `pgee.prototype.emit()` method triggers a `NOTIFY` on the database channel.
+
+```javascript
+const CONNECT_STRING = `postgres://${process.env.USER}@localhost/postgres`;
+const PgEe = require('pgee');
+
+// The constructor accepts a pg.Client, connection string, or any valid
+// pg connection configuration.
+const pgee = new PgEe(CONNECT_STRING);
+
+pgee.connect((err) => {
+  if (err) {
+    return console.error(err);
+  }
+
+  // Listen on "foo" channel. When a notification is received, emit a "foo"
+  // event, triggering the listener function.
+  pgee.listen({
+    channel: 'foo',
+    listener: (data) => {
+      console.log(`Received 'foo' event: ${JSON.stringify(data)}`);
+    }
+  }, (err, channel) => {
+    if (err) {
+      return console.error(err);
+    }
+
+    pgee.emit('foo', {bar: 'baz'});
+  });
+});
+```
+
+## API
+
+### `pgee(options)` Constructor
+
+  - Arguments
+    - `options` (varies) - Options to pass to `pg.connect()` or a connected instance of `pg.Client` (see the [`pg` module](https://www.npmjs.com/package/pg) for details).
+  - Returns
+    - object - A newly constructed `pgee` instance.
+
+Constructs a new `pgee` instance, which extends `EventEmitter`. The constructor does not attempt to connect to a database.
+
+### `pgee.prototype.connect([callback])`
+
+  - Arguments
+    - `callback` (function) - An optional callback function that is invoked after connecting to the database. The callback only receives a possible error argument.
+  - Returns
+    - Nothing
+
+Connects to a PostgreSQL database using the credentials provided to the constructor. The connection is taken from the `pg` module's connection pool. If an instance of `pg.Client` was passed to the constructor, it is assumed to already be connected, so no further action is taken. If `callback` is a function, it is called upon completion. If a function was not provided, a `'connect'` event is emitted.
+
+### `pgee.prototype.close()`
+
+  - Arguments
+    - None
+  - Returns
+    - Nothing
+
+Closes the `pgee` instance. If the connection was taken from the `pg` connection pool during `connect()`, it will be returned to the pool. If an external `pg.Client` was provided to the constructor, no action will be taken on the client. All attached event listeners are removed from the instance.
+
+### `pgee.prototype.listen(channel [, callback])`
+
+  - Arguments
+    - `channel` (string or object) - If this is a string, it represents the name of the channel to subscribe to. If is is an option, it supports the following properties.
+      - `channel` (string) - The channel name to subscribe to.
+      - `listener` (function) - An optional event listener that triggers on `channel` events. The data associated with the database notification is passed as the only argument.
+    - `callback` (function) - An optional callback function. If an error occurs, it is passed as the first argument of the callback. On success, the second argument of the callback is the channel name as a string.
+  - Returns
+    - Nothing
+
+Issues a `LISTEN "channel"` to the database. If a `listener` is provided, it is attached using the `on()` method. Additional listeners can be attached later using `on()`, `addListener()`, `once()`, or `listen()`. After the `LISTEN` command is issued, the callback function is invoked (if one is present). If no callback was provided, a `'listen'` event is emitted, containing the name of the channel that was added.
+
+### `pgee.prototype.unlisten(channel [, callback])`
+
+  - Arguments
+    - `channel` (string or object) - If this is a string, it represents the name of the channel to unsubscribe from. If is is an option, it supports the following properties.
+      - `channel` (string) - The channel name to subscribe to.
+      - `removeListeners` (boolean) - An optional value that, if `true`, removes all `channel` listeners. Defaults to `false`.
+    - `callback` (function) - An optional callback function. If an error occurs, it is passed as the first argument of the callback. On success, the second argument of the callback is the channel name as a string.
+  - Returns
+    - Nothing
+
+Issues an `UNLISTEN "channel"` to the database. If `removeListeners` is true, any existing `channel` listeners are removed using `removeAllListeners(channel)`. Listeners can also be removed later using `removeAllListeners()`, `unlisten()`, etc. After the `UNLISTEN` command is issued, the callback function is invoked (if one is present). If no callback was provided, an `'unlisten'` event is emitted, containing the name of the channel that was removed.
+
+### `pgee.prototype.emit(event, message)`
+
+  - Arguments
+    - `event` (string) - Name of the event to emit. If the event name is `'error'`, an error is emitted immediately. Any other value triggers a database notification, where the channel name is equal to `event`.
+    - `message` (varies) - Data attached to the emitted event.
+  - Returns
+    - Nothing
+
+Issues a `pg_notify()` to the database, where the channel name is `event`, and `message` represents the notification payload. If `event` is `'error'`, then no request is made to the database, and an error is immediately emitted. Note that the names `'connect'`, `'listen'`, and `'unlisten'` have the potential to conflict with other events used in `pgee`.
---
+++ test/index.js
@@ -0,0 +1,466 @@
+'use strict';
+const Code = require('code');
+const Lab = require('lab');
+const Postgresql = require('pg');
+const PgEe = require('../lib');
+const CONNECT_STRING = process.env.POSTGRESQL_CONNECTION ||
+                       `postgres://${process.env.USER}@localhost/postgres`;
+
+const lab = exports.lab = Lab.script();
+const expect = Code.expect;
+const describe = lab.describe;
+const it = lab.it;
+
+describe('pgee', () => {
+  lab.after((done) => {
+    Postgresql.end();
+    done();
+  });
+
+  describe('PgEe()', () => {
+    it('accepts a Postgresql client as input', (done) => {
+      const client = new Postgresql.Client(CONNECT_STRING);
+      const pgee = new PgEe(client);
+
+      expect(pgee._connection).to.equal(client);
+      expect(pgee._options).to.equal(null);
+      expect(pgee._done).to.equal(null);
+      expect(pgee._channels).to.deep.equal([]);
+      expect(pgee.getMaxListeners()).to.equal(Infinity);
+      done();
+    });
+
+    it('accepts Postgresql connection information as input', (done) => {
+      const pgee = new PgEe(CONNECT_STRING);
+
+      expect(pgee._connection).to.equal(null);
+      expect(pgee._options).to.equal(CONNECT_STRING);
+      expect(pgee._done).to.equal(null);
+      expect(pgee._channels).to.deep.equal([]);
+      expect(pgee.getMaxListeners()).to.equal(Infinity);
+      done();
+    });
+  });
+
+  describe('PgEe.prototype.connect()', (done) => {
+    it('returns early if connection already exists', (done) => {
+      const client = new Postgresql.Client(CONNECT_STRING);
+      const pgee = new PgEe(client);
+      const originalConnect = Postgresql.connect;
+
+      Postgresql.connect = (options, callback) => {
+        Postgresql.connect = originalConnect;
+        callback(new Error('foo'));
+      };
+
+      pgee.on('error', (err) => {
+        Code.fail(err);
+      });
+
+      pgee.on('connect', () => {
+        Postgresql.connect = originalConnect;
+        done();
+      });
+
+      pgee.connect((err) => {
+        expect(err).to.not.exist();
+        pgee.connect();
+      });
+    });
+
+    it('uses Postgresql thread pool if no connection is provided', (done) => {
+      const pgee = new PgEe(CONNECT_STRING);
+
+      expect(pgee._connection).to.not.exist();
+      pgee.connect((err) => {
+        expect(err).to.not.exist();
+        expect(pgee._connection instanceof Postgresql.Client).to.equal(true);
+        pgee.close();
+        done();
+      });
+    });
+
+    it('handles errors from Postgresql.connect()', (done) => {
+      const pgee = new PgEe(CONNECT_STRING);
+      const originalConnect = Postgresql.connect;
+
+      Postgresql.connect = (options, callback) => {
+        Postgresql.connect = originalConnect;
+        callback(new Error('foo'));
+      };
+
+      expect(pgee._connection).to.not.exist();
+      pgee.connect((err) => {
+        expect(err.message).to.equal('foo');
+        expect(pgee._connection).to.not.exist();
+        done();
+      });
+    });
+
+    it('handles errors from the connection', (done) => {
+      const client = new Postgresql.Client(CONNECT_STRING);
+      const pgee = new PgEe(client);
+
+      pgee.on('error', (err) => {
+        expect(err.message).to.equal('foo');
+        pgee.close();
+        done();
+      });
+
+      pgee.connect((err) => {
+        expect(err).to.not.exist();
+        pgee._connection.emit('error', new Error('foo'));
+      });
+    });
+  });
+
+  describe('PgEe.prototype.listen()', (done) => {
+    it('listens to channels in database', (done) => {
+      const pgee = new PgEe(CONNECT_STRING);
+
+      pgee.connect((err) => {
+        expect(err).to.not.exist();
+        expect(pgee._channels).to.deep.equal([]);
+
+        // Handle a string
+        pgee.listen('foo', (err, channel) => {
+          expect(err).to.not.exist();
+          expect(channel).to.equal('foo');
+          expect(pgee._channels).to.deep.equal(['foo']);
+          expect(pgee.listenerCount('foo')).to.equal(0);
+
+          // Handle an object
+          pgee.listen({
+            channel: 'bar',
+            listener: (data) => {}
+          }, (err, channel) => {
+            expect(err).to.not.exist();
+            expect(channel).to.equal('bar');
+            expect(pgee._channels).to.only.include(['foo', 'bar']);
+            expect(pgee.listenerCount('foo')).to.equal(0);
+            expect(pgee.listenerCount('bar')).to.equal(1);
+
+            // Handle a duplicate with a listener
+            pgee.listen({
+              channel: 'foo',
+              listener: (data) => {}
+            }, (err, channel) => {
+              expect(err).to.not.exist();
+              expect(channel).to.equal('foo');
+              expect(pgee._channels).to.only.include(['foo', 'bar']);
+              expect(pgee.listenerCount('foo')).to.equal(1);
+              expect(pgee.listenerCount('bar')).to.equal(1);
+
+              // Handle a duplicate without a listener
+              pgee.listen('bar', (err, channel) => {
+                expect(err).to.not.exist();
+                expect(channel).to.equal('bar');
+                expect(pgee._channels).to.only.include(['foo', 'bar']);
+                expect(pgee._channels.length).to.equal(2);
+                expect(pgee.listenerCount('foo')).to.equal(1);
+                expect(pgee.listenerCount('bar')).to.equal(1);
+                pgee.close();
+                done();
+              });
+            });
+          });
+        });
+      });
+    });
+
+    it('treats primitive channel names as strings', (done) => {
+      const pgee = new PgEe(CONNECT_STRING);
+
+      pgee.connect((err) => {
+        expect(err).to.not.exist();
+        pgee.listen(null, (err, channel) => {
+          expect(err).to.not.exist();
+          expect(channel).to.equal('null');
+          expect(pgee._channels).to.deep.equal(['null']);
+          expect(pgee.listenerCount('null')).to.equal(0);
+          pgee.close();
+          done();
+        });
+      });
+    });
+
+    it('handles database errors', (done) => {
+      const pgee = new PgEe(CONNECT_STRING);
+
+      pgee.connect((err) => {
+        expect(err).to.not.exist();
+        expect(pgee._channels).to.deep.equal([]);
+
+        const originalQuery = pgee._connection.query;
+
+        pgee._connection.query = (sql, callback) => {
+          pgee._connection.query = originalQuery;
+          return callback(new Error('bar'));
+        };
+
+        pgee.listen('foo', (err, channels) => {
+          expect(err.message).to.equal('bar');
+          expect(channels).to.not.exist();
+          expect(pgee._channels).to.deep.equal([]);
+          pgee.close();
+          done();
+        });
+      });
+    });
+
+    it('errors if not connected', (done) => {
+      const pgee = new PgEe(CONNECT_STRING);
+
+      pgee.listen('foo', (err, channels) => {
+        expect(err instanceof Error).to.equal(true);
+        expect(channels).to.not.exist();
+        done();
+      });
+    });
+  });
+
+  describe('PgEe.prototype.unlisten()', (done) => {
+    it('unlistens to channels in database', (done) => {
+      const pgee = new PgEe(CONNECT_STRING);
+
+      pgee.connect((err) => {
+        expect(err).to.not.exist();
+        pgee.listen('foo', (err, channel) => {
+          expect(err).to.not.exist();
+          pgee.listen({
+            channel: 'bar',
+            listener: () => {}
+          }, (err, channel) => {
+            expect(err).to.not.exist();
+            expect(pgee._channels.length).to.equal(2);
+            expect(pgee.listenerCount('foo')).to.equal(0);
+            expect(pgee.listenerCount('bar')).to.equal(1);
+
+            // Handle a string
+            pgee.unlisten('foo', (err, channel) => {
+              expect(err).to.not.exist();
+              expect(channel).to.equal('foo');
+              expect(pgee._channels).to.only.include('bar');
+              expect(pgee.listenerCount('bar')).to.equal(1);
+
+              // Handle an object that removes listeners
+              pgee.unlisten({
+                channel: 'bar',
+                removeListeners: true
+              }, (err, channel) => {
+                expect(err).to.not.exist();
+                expect(channel).to.equal('bar');
+                expect(pgee._channels).to.deep.equal([]);
+                expect(pgee.listenerCount('bar')).to.equal(0);
+                pgee.close();
+                done();
+              });
+            });
+          });
+        });
+      });
+    });
+
+    it('unlistens channels that aren\'t being listened to', (done) => {
+      const pgee = new PgEe(CONNECT_STRING);
+
+      pgee.on('foo', () => {});
+      pgee.on('bar', () => {});
+
+      pgee.connect((err) => {
+        expect(err).to.not.exist();
+        pgee.unlisten('foo', (err, channel) => {
+          expect(err).to.not.exist();
+          expect(channel).to.equal('foo');
+          expect(pgee._channels).to.deep.equal([]);
+          expect(pgee.listenerCount('foo')).to.equal(1);
+          expect(pgee.listenerCount('bar')).to.equal(1);
+
+          pgee.unlisten({
+            channel: 'bar',
+            removeListeners: true
+          }, (err, channel) => {
+            expect(err).to.not.exist();
+            expect(channel).to.equal('bar');
+            expect(pgee._channels).to.deep.equal([]);
+            expect(pgee.listenerCount('foo')).to.equal(1);
+            expect(pgee.listenerCount('bar')).to.equal(0);
+            pgee.close();
+            done();
+          });
+        });
+      });
+    });
+
+    it('treats primitive channel names as strings', (done) => {
+      const pgee = new PgEe(CONNECT_STRING);
+
+      pgee.connect((err) => {
+        expect(err).to.not.exist();
+        pgee.unlisten(null, (err, channel) => {
+          expect(err).to.not.exist();
+          expect(channel).to.equal('null');
+          expect(pgee._channels).to.deep.equal([]);
+          expect(pgee.listenerCount('null')).to.equal(0);
+          pgee.close();
+          done();
+        });
+      });
+    });
+
+    it('handles database errors', (done) => {
+      const pgee = new PgEe(CONNECT_STRING);
+
+      pgee.connect((err) => {
+        expect(err).to.not.exist();
+        pgee.listen('foo', (err, channels) => {
+          expect(err).to.not.exist();
+          expect(pgee._channels).to.only.include('foo');
+
+          const originalQuery = pgee._connection.query;
+
+          pgee._connection.query = (sql, callback) => {
+            pgee._connection.query = originalQuery;
+            return callback(new Error('bar'));
+          };
+
+          pgee.unlisten('foo', (err, channels) => {
+            expect(err.message).to.equal('bar');
+            expect(channels).to.not.exist();
+            expect(pgee._channels).to.deep.equal(['foo']);
+            pgee.close();
+            done();
+          });
+        });
+      });
+    });
+
+    it('errors if not connected', (done) => {
+      const pgee = new PgEe(CONNECT_STRING);
+
+      pgee.unlisten('foo', (err, channels) => {
+        expect(err instanceof Error).to.equal(true);
+        expect(channels).to.not.exist();
+        done();
+      });
+    });
+  });
+
+  describe('PgEe.prototype.emit()', (done) => {
+    it('emits channel messages as events', (done) => {
+      const pgee = new PgEe(CONNECT_STRING);
+      const message = {bar: 'baz'};
+
+      pgee.on('foo', (data) => {
+        expect(data).to.deep.equal(message);
+        pgee.close();
+        done();
+      });
+
+      pgee.connect((err) => {
+        expect(err).to.not.exist();
+        pgee.listen('foo', (err, channels) => {
+          expect(err).to.not.exist();
+          pgee.emit('foo', message);
+        });
+      });
+    });
+
+    it('can emit error events without being connected', (done) => {
+      const pgee = new PgEe(CONNECT_STRING);
+      const error = new Error('foo');
+
+      pgee.on('error', (err) => {
+        expect(err).to.equal(error);
+        done();
+      });
+
+      pgee.emit('error', error);
+    });
+
+    it('errors if not connected', (done) => {
+      const pgee = new PgEe(CONNECT_STRING);
+
+      pgee.on('error', (err) => {
+        expect(err instanceof Error).to.equal(true);
+        done();
+      });
+
+      pgee.emit('foo', 'bar');
+    });
+
+    it('handles database errors', (done) => {
+      const pgee = new PgEe(CONNECT_STRING);
+
+      pgee.on('error', (err) => {
+        expect(err.message).to.equal('bar');
+        pgee.close();
+        done();
+      });
+
+      pgee.on('foo', () => {
+        Code.fail('unexpected foo event');
+      });
+
+      pgee.connect((err) => {
+        expect(err).to.not.exist();
+        pgee.listen('foo', (err, channels) => {
+          expect(err).to.not.exist();
+
+          const originalQuery = pgee._connection.query;
+
+          pgee._connection.query = (sql, params, callback) => {
+            pgee._connection.query = originalQuery;
+            return callback(new Error('bar'));
+          };
+
+          pgee.emit('foo', {baz: '123'});
+        });
+      });
+    });
+  });
+
+  describe('PgEe.prototype.close()', (done) => {
+    it('closes the database connection', (done) => {
+      const pgee = new PgEe(CONNECT_STRING);
+
+      pgee.on('error', (err) => {
+        Code.fail(err);
+      });
+
+      pgee.connect((err) => {
+        expect(err).to.not.exist();
+        expect(pgee.listenerCount('error')).to.equal(1);
+        pgee.close();
+        expect(pgee._connection).to.equal(null);
+        expect(pgee._done).to.equal(null);
+        expect(pgee.listenerCount('error')).to.equal(0);
+        done();
+      });
+    });
+
+    it('does not modify an external database connection', (done) => {
+      const client = new Postgresql.Client(CONNECT_STRING);
+      const pgee = new PgEe(client);
+
+      expect(pgee._connection instanceof Postgresql.Client).to.equal(true);
+      expect(pgee._done).to.equal(null);
+      pgee.close();
+      expect(client instanceof Postgresql.Client).to.equal(true);
+      expect(pgee._connection).to.equal(null);
+      expect(pgee._done).to.equal(null);
+      done();
+    });
+
+    it('returns early if not connected', (done) => {
+      const pgee = new PgEe(CONNECT_STRING);
+
+      pgee._done = () => {
+        Code.fail('should not be called');
+      };
+
+      pgee.close();
+      done();
+    });
+  });
+});
